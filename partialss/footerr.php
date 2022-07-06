ed958b4fc485a31add3caa3f00d84a58c04a3787
sahitiputcha <92791275+sahitiputcha@users.noreply.github.com>
2022-07-06 19:29:07 +0530

    Create footerr.php

---
+++ partialss/footerr.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a class="link" href="#home"> <i class="fas fa-angle-right"></i> home</a>
+            <a class="link" href="#shop"> <i class="fas fa-angle-right"></i> shop</a>
+            
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#" class="link"> <i class="fas fa-angle-right"></i> my order </a>
+            <a href="#" class="link"> <i class="fas fa-angle-right"></i> my favorite </a>
+            <a href="#" class="link"> <i class="fas fa-angle-right"></i> my wishlist </a>
+            <a href="#" class="link"> <i class="fas fa-angle-right"></i> private policy </a>
+            <a href="#" class="link"> <i class="fas fa-angle-right"></i> terms of use </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +918247803623</p>
+            <p> <i class="fas fa-phone"></i> +918985885755 </p>
+            <p> <i class="fas fa-phone"></i> +919398397835 </p>
+            <p> <i class="fas fa-envelope"></i> sahitiputcha2043@gmail.com </p>
+            <p> <i class="fas fa-map"></i> Hyderabad, India - 500001 </p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                
+            </div>
+        </div>
+
+    
+    </div>
+
+    <div class="credit"> created by <span> Glow Cosmetics Team</span> | all rights reserved! </div>
+
+</section>
