19776da3c4c5c360ddb4391930519bd5a6cd9a82
sahitiputcha <92791275+sahitiputcha@users.noreply.github.com>
2022-07-06 19:23:41 +0530

    Create footer.php

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!-- footer section starts  -->
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022 All rights reserved ,GLOW cosmetics, Developed by <a href="a"> Satvika Putcha,Sahiti Putcha,Sai Kiran</a></p>
+        </div> 
+    
+    </div>
+    <!-- footer  section ends  -->
+
+
+
+    </html>
