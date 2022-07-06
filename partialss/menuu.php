f47073cedd84df7d7d5f7aae3ce54ee7646a73d2
sahitiputcha <92791275+sahitiputcha@users.noreply.github.com>
2022-07-06 19:29:29 +0530

    Create menuu.php

---
+++ partialss/menuu.php
@@ -0,0 +1,32 @@
+<header class="header">
+
+    <a href="#home" class="title">GLOW </a>
+   
+
+    <form action="" class="search-form">
+        <input type="search" name="" placeholder="search here..." id="search-box">
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+
+    <div class="icons">
+        <div id="menu-btn" class="fas fa-bars"></div>
+        <div id="search-btn" class="fas fa-search"> </div>
+        <a href="wishlist.php" class="fas fa-heart"></a>
+        <a href="cart.php" class="fas fa-shopping-cart"> 
+
+        </a>
+    </div>
+
+</header>
+
+<!-- navbar section  -->
+
+<nav class="navbar">
+   
+    <div id="close-navbar" class="fas fa-times"></div>
+    <a href="#home">home</a>
+    <a href="#shop">shop</a>
+    <a href="usersignup.php">Logout</a>
+    
+    
+</nav>
