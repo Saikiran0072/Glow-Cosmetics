1310efd70eac6b5547180aeb087b761c00998397
sahitiputcha <92791275+sahitiputcha@users.noreply.github.com>
2022-07-06 19:24:06 +0530

    Create menu.php

---
+++ admin/partials/menu.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+    <title>Admin page</title>
+    <link rel="stylesheet" href="../css/adminn.css">
+</head>
+
+<body>
+</body>
+  <!-- main page section starts -->
+
+  <div class="menu" >
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="mange-product.php">Product</a></li>
+                <li><a href="manage-order.php">Order</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div> 
+    </div>
+
+    <!-- main page section ends  -->
