97162c0a6fef69bb11465a58aec39c8853bb9810
sahitiputcha <92791275+sahitiputcha@users.noreply.github.com>
2022-07-06 19:21:32 +0530

    Create manage-order.php

---
+++ admin/manage-order.php
@@ -0,0 +1,45 @@
+<?php 
+include('partials/menu.php');?>
+<div class ="main-content">
+        <div class="wrapper">
+        <h1>Manage Order</h1>
+        <br/>
+       
+        <br/> <br/>
+        
+            <table class="tbl-full">
+                <tr>
+                    <th>S.No.</th>
+                    <th>full name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <br/>
+                <tr>
+                    <td>1.</td>
+                    <td>Sahiti Putcha </td>
+                    <td>sahiti</td>
+                    <td>
+                    <a href="a" class="btn-secondary">Update Admin </a>
+                    <a href="a" class="btn-secondary">Delete Admin </a>    
+                    </td>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Sahiti Putcha </td>
+                    <td>sahiti</td>
+                    <td>
+                    <a href="a" class="btn-secondary">Update Admin </a>
+                    <a href="a" class="btn-secondary">Delete Admin </a>    
+                    </td>
+                </tr>
+                
+            </table>
+        </div> 
+    </div>
+    <!-- main content section ends  -->
+
+<?php
+include('partials/footer.php');
+?>
+
